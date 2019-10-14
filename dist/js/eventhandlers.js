772aebaa9e9127d8218563c83c74971770880adc
Jason Diep <jdiep1@sheffield.ac.uk>
2019-10-14 18:23:39 +0100

    building product bar

--- dist/js/eventhandlers.js
+++ dist/js/eventhandlers.js
@@ -1,6 +1,7 @@
 // Select DOM Items
 
 const menuBtn = document.querySelector(".menu-btn");
+const linkBtn = document.querySelectorAll(".nav-link");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
@@ -10,9 +11,11 @@ const navItems = document.querySelectorAll(".nav-item");
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+linkBtn.forEach(item => item.addEventListener("click", toggleMenu));
 
 function toggleMenu() {
   menuBtn.classList.toggle("close");
+  linkBtn.forEach(item => item.classList.toggle("close"));
   menu.classList.toggle("show");
   menuNav.classList.toggle("show");
   navItems.forEach(item => item.classList.toggle("show"));
