df5e4ae1438919a1c2876a75756e4fb6654785be
Jason Diep <jdiep1@sheffield.ac.uk>
2019-10-16 15:20:25 +0100

    second draft menu items prices do not match in mobile view

--- dist/js/eventhandlers.js
+++ dist/js/eventhandlers.js
@@ -22,25 +22,27 @@ function toggleMenu() {
 
 // Product Bar
 
-document.getElementById("veg").onclick = function() {
-  insertMenuText(vegStarterDishes);
-  insertPricesText(vegStarterPrices);
-};
-
 var menuItems, menuItemPrices;
 var vegStarterDishes =
   "<div><h4>Vegetarian Starters</h4><li>1. Vegetarian Spring Rolls</li><li>2. Crispy Seaweed</li><li>3. Vegetable Hot & Sour Soup</li><li>4. Mixed Vegetable Soup</li><li>5. Sweetcorn Soup</li></div>";
 var vegStarterPrices =
-  "<div><li>-----</li><li>£3.50</li><li>£3.50</li><li>£2.50</li><li>£2.50</li><li>£2.50</li></div>";
+  "<div class='price-right'><li>-----</li><li>£3.50</li><li>£3.50</li><li>£2.50</li><li>£2.50</li><li>£2.50</li></div>";
+var vegMainDishes =
+  "<div><h4>Vegetarian Main Courses</h4><li>6. Braised Chinese Vegetables and Chinese Mushrooms in Oyster Sauce</li><li>7. Deep Fried Bean Curd with Vegetables and Chinese Mushrooms</li><li>8. Stir Fried Mixed Vegetables with Garlic or Oyster Sauce</li><li>9. Fried Vegetables with Cashewnuts (N)</li><li>10. Fried Vegetables in Garlic & Chilli Sauce</li><li>11. Braised Bean Curd in Chilli & Garlic or Sweet & Sour Sauce</li><li>12. Deep Fried Bean Curd in Chilli & Garlic or Sweet & Sour Sauce</li><li>13. Stir Fried Bamboo Shoots & Water Chestnuts in Hot Bean Sauce (N)</li><li>14. Stir Fried Beancurd in Kindo Sauce (N)</li><li>15. Deep Fried Salt & Peppered Bean Curd</li><li>16. Vegetarian Singapore Vermicelli (Spicy)</li><li>17. Vegetable Chow Mein or Ho Fan</li></div>";
+var vegMainPrices =
+  "<div class='price-right'><li>-----</li><li>£6.00</li><li>£6.00</li><li>£6.00</li><li>£6.50</li><li>£6.00</li><li>£6.00</li><li>£6.00</li><li>£6.00</li><li>£6.00</li><li>£6.00</li><li>£6.50</li><li>£6.50</li></div>";
+
+document.getElementById("veg").onclick = function() {
+  menuItems = [];
+  insertMenuText(vegStarterDishes);
+  insertMenuText(vegStarterPrices);
+  insertMenuText(vegMainDishes);
+  insertMenuText(vegMainPrices);
+};
 
 function insertMenuText(value) {
-  menuItems = value;
+  menuItems += value;
   document.getElementById("product-list").innerHTML = menuItems;
-}
-
-function insertPricesText(value) {
-  menuItemPrices = value;
-  document.getElementById("product-list").innerHTML += menuItemPrices;
 }
 
 //Media Queries
